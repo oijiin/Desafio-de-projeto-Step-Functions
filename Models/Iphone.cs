a7c32eebdf97020a3220f29cc48ac95d60b78983
juniorlemos <47072463+juniorlemos@users.noreply.github.com>
2024-04-18 14:50:27 -0300

    Update Iphone.cs
    Atualização da classe Iphne

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,11 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
+    
+public override void InstalarAplicativo(string nomeApp)
+        {
+            // Implementa a lógica específica para instalar um aplicativo em um iPhone
+            Console.WriteLine($"Instalando aplicativo {nomeApp} na App Store do iPhone...");
+        }    }
 }
