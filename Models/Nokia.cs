228aa2a3d9fb12f1dcb6ebd9157936478c67fd85
juniorlemos <47072463+juniorlemos@users.noreply.github.com>
2024-04-18 16:34:12 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -2,7 +2,11 @@ namespace DesafioPOO.Models
 {
     public class Nokia : Smartphone
     {
-    
+     public Nokia(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+            // Qualquer inicialização específica para Nokia pode ser feita aqui
+        }
 public override void InstalarAplicativo(string nomeApp)
         {
             // Implementa a lógica específica para instalar um aplicativo em um Nokia
