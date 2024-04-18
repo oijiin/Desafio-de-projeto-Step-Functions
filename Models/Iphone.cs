64e932062fc4bb76cf329fc8302b3e95cad3af64
juniorlemos <47072463+juniorlemos@users.noreply.github.com>
2024-04-18 16:35:01 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,10 +3,8 @@ namespace DesafioPOO.Models
     public class Iphone : Smartphone
     {
  public Iphone(string numero, string modelo, string imei, int memoria) 
-            : base(numero, modelo, imei, memoria)
-        {
-            // Qualquer inicialização específica para Nokia pode ser feita aqui
-        }public override void InstalarAplicativo(string nomeApp)
+            : base(numero, modelo, imei, memoria){}
+            public override void InstalarAplicativo(string nomeApp)
         {
             // Implementa a lógica específica para instalar um aplicativo em um iPhone
             Console.WriteLine($"Instalando aplicativo {nomeApp} na App Store do iPhone...");
