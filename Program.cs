aee58de792653327345769b0fab9597d524b1c96
Lucas Nishikawara <lsnishikawara@gmail.com>
2024-05-07 12:25:59 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123","2780 Flip","0000-ZZZZ", 128);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Play store");
+
+Smartphone iphone = new Iphone("456", "Iphone 17", "ZZZZ-0000", 128);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("App store");
