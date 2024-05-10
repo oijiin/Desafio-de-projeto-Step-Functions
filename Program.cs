5039673bf08c565186df207d7eea13ad9db68999
Williams Falcão <williamsfalcao@yahoo.com.br>
2024-05-10 20:35:34 -0300

    resolucao

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("12345678", "NK001", "123321", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("87654321", "PRO-MAX14", "321123", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
