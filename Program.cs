460b7f797e65eeeb537cc78d14a193fd0249b33c
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-05-10 08:31:07 -0300

    Adicionando ReceberLigação em Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "0987655", modelo: "Modelo 1", imei: "111111111", memoria: 128);
 Smartphone.Ligar();
+Smartphone.ReceberLigacao();
 nokia.InstalarAplicativo("Instagram");
 
 Console.WriteLine("Encerrando teste em Nokia.\n");
@@ -11,6 +12,7 @@ Console.WriteLine("Encerrando teste em Nokia.\n");
 Console.WriteLine("Smartphone Iphone:");
 Smartphone iphone = new Iphone(numero: "93456785", modelo: "Modelo 2", imei: "22222222", memoria: 64);
 Smartphone.Ligar();
+Smartphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
 
 Console.WriteLine("Encerrando teste em Iphone.\n");
