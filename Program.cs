e736ca767e57d9ed4813d05499719d1cb4b3519f
Iago Alves <iago20700@gmail.com>
2024-05-11 17:13:00 -0300

    DESAFIO DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("1234567890", "Modelo X", "IMEI123456", 1000);
+nokia.Ligar();
+nokia.InstalarAplicativo("WHATSAPP");
+
+
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("44444444", "Modelo X5", "444444", 10050);
+iphone.Ligar();
+iphone.InstalarAplicativo("TELEGRAM");
+
