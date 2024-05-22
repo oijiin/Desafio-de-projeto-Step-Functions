f04aada8000cf8b4c4bbfd224e11c951bbe54a26
Wanderson Gomes <wanderson.gomes@iob.com.br>
2024-05-22 12:18:30 -0300

    Desafio concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    //IMPLEMENTADO
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        //IMPLEMENTADO
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        { }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando aplicativo em um nokia");
+        }
     }
 }
