87c296dcfdd70928a6e1ec6b21b38c6b1f4d075d
Jean Santos de Jesus <jean.jesus@mpba.mp.br>
2024-06-03 14:29:22 -0300

    correcao feita

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -4,10 +4,7 @@ namespace DesafioPOO.Models
     {
         public Iphone (string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
         {
-            //Numero = numero;
-            //Modelo = modelo;
-            //IMEI = imei;
-            //Memoria = memoria;
+
         }
 
         public override void InstalarAplicativo(string nomeApp)
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,10 +4,7 @@ namespace DesafioPOO.Models
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            //Numero = numero;
-            //Memoria = memoria;
-            //IMEI = imei;
-            //Memoria = memoria;
+           
         }
         public override void InstalarAplicativo(string nomeApp)
         {
