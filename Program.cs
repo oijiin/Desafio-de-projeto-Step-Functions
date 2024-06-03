1078345c9ddde45d7ba5b8a01f852f2fa470cf51
Jean Santos de Jesus <jean.jesus@mpba.mp.br>
2024-06-03 14:22:03 -0300

    projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("111", "1973", "7asgda7g", 49);
+Iphone iphone = new Iphone("222", "7S", "asd2g2782", 27);
+
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
+iphone.Ligar();
