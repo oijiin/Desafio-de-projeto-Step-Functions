5c5cc17c37e6b5cf9ca7186c12b36e7a7f572fec
Douglas Rodrigues dos Santos <doug_rs@outlook.com>
2024-06-03 21:50:32 -0300

    Implementação do sistema de abstração de celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            Console.WriteLine($"{nokia.Numero}, {nokia.Modelo}, {nokia.IMEI}, {nokia.Memoria}");
+            //nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+            Console.WriteLine($"{iphone.Numero}, {iphone.Modelo}, {iphone.IMEI}, {iphone.Memoria}");
+            iphone.Ligar();
+            //.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
