3370298fabb9c716d992d80a0ab505f4d4f32f6d
Tiago Silva <tiagosilva@bemol.com.br>
2024-06-24 16:29:19 -0300

    Desafio Realizado com Sucesso

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int MemoriaInterna { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoriaInterna)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            MemoriaInterna = memoriaInterna;
         }
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+            Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
