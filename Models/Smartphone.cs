58ae8fef8db721981f6c2001322d424344a071e2
moacir <moacir9401@gmail.com>
2024-07-03 00:00:50 -0300

    Atualizações nas classes Smartphone, Iphone e Nokia
    Implementada herança na classe `Smartphone` para as classes `Iphone` e `Nokia`, permitindo o compartilhamento de métodos e propriedades. Os construtores dessas classes foram atualizados para aceitar novos parâmetros (`numero`, `modelo`, `imei`, `memoriaInterna`), que são passados ao construtor da classe `Smartphone`. Além disso, o método `InstalarAplicativo` foi sobrescrito em `Iphone` e `Nokia` para permitir mensagens de instalação específicas. Na classe `Smartphone`, adicionadas propriedades protegidas (`Modelo`, `IMEI`, `MemoriaInterna`) e atualizado o construtor para inicializar essas propriedades. Incluídos testes para `Nokia` e `Iphone` no arquivo `Program.cs`, testando criação de instâncias e chamadas a métodos específicos.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int MemoriaInterna { get; set; } 
+         
+        protected Smartphone(string numero, string modelo, string imei, int memoriaInterna)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            MemoriaInterna = memoriaInterna;
         }
 
         public void Ligar()
