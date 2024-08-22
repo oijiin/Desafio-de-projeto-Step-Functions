fd3348fe3968fb23aea3c7d5f18072fe1ad05e62
Pedro Vale <ph40200v@gmail.com>
2024-08-22 15:03:00 -0300

    Descrição das mudanças

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com Nokia
+        Smartphone nokia = new Nokia("123456789", "Nokia Modelo", "111111111111", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Teste com Iphone
+        Smartphone iphone = new Iphone("987654321", "iPhone Modelo", "222222222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
