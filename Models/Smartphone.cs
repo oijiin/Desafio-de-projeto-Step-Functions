c7e1d3cbd120ae525beb20ca677e4f6b1e9ed74c
elisangeloalves <preto115@gmail.com>
2024-08-26 12:27:59 -0300

    feat: implementação do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,23 +4,40 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _iMEI;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string iMEI, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _iMEI = iMEI;
+            _memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando...\t");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação...\t");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        /*
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine("Detalhes do Smartphone:");
+            Console.WriteLine($"\tNúmero: {Numero}");
+            Console.WriteLine($"\tModelo: {_modelo}");
+            Console.WriteLine($"\tIMEI: {_iMEI}");
+            Console.WriteLine($"\tMemória: {_memoria} MB");
+        }
+        */
     }
 }
