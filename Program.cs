c69d6ceb3d90cb8b27e1fca49888a9dd50c82ee7
César L. Guilherme <clguilherme3@gmail.com>
2024-08-28 17:44:18 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+    // TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("Smatphone Nokia");
+        Nokia nokia = new Nokia("123456789", "Lumia 950", "123456789012345", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Alura");
+
+        Console.WriteLine("/n");
+
+        Console.WriteLine("Smatphone iPhone");
+        Iphone iphone = new Iphone("987654321", "13", "987654321098765", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Dio");
+    }
+}
