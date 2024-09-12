250ce9d8d542500fae67e1e6fd0a8cd3ec5dabd4
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-09-12 17:16:55 -0300

    projeto concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string memoria, string iMEI) : base(numero, modelo, memoria, iMEI)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine(nomeApp);
+        }
     }
 }
