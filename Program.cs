19be72788837c7dd23429c65d44ab90c7954fce4
arthurevg <arthurevangelista05@gmail.com>
2024-09-17 14:30:22 -0300

    exercicio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Realizando os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone nokia:");
+Nokia n = new Nokia("61912345678", "modelo 1", "1111111", 128);
+n.Ligar();
+n.InstalarAplicativo("Roblox");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone i = new Iphone("61987654321", "modelo 2", "2222222", 64);
+i.ReceberLigacao();
+i.InstalarAplicativo("Whatsapp");
