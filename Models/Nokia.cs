c68f7e92533b84a77705199390cbd0790204b49f
Raphael Santos <raphaelfreitassantos7@gmail.com>
2024-09-17 22:03:40 -0300

    Atulização v1.1

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,23 @@
+using DesafioPOO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Istalando o aplicativo {nomeApp} no Nokia");
+        }
     }
 }
