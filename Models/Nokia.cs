9adec5b1e4f66174c9eaf14cbf412ff2ffd8976d
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-09-26 06:10:02 -0300

    Add Herencia de Smartphone y sobrescritura de InstalarAplicativo en Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,21 @@
+using System.ComponentModel;
+using DesafioPOO.Models;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor que passa os parâmetros para o construtor da classe base (Smartphone)
+        public Nokia(string numero, string modelo, string imei, int memoria)
+        : base(numero, modelo, imei, memoria) 
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} Nokia");
+        }
     }
 }
