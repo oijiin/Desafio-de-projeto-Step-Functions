fb2de08e933288ec83ba40a0eb94bf50d354c99b
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-09-26 06:16:26 -0300

    Add Herencia de Smartphone y sobrescritura de InstalarAplicativo en Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdando da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor que passa os parâmetros para o construtor da classe base (Smartphone)
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescrevendo o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone");
+        }
     }
 }
