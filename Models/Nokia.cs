4aacee8bb3652582d6e0f48276fc6477cba11950
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-09-26 06:30:37 -0300

    Add pruebas a las clases heredadas del Smartphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -15,7 +15,7 @@ namespace DesafioPOO.Models
 
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando o aplicativo {nomeApp} Nokia");
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
         }
     }
 }
