b2be60973f3ac322df32279caf3aba50c57fc6c8
reboucasdi <diegoreboucas.santana3@gmail.com>
2024-09-28 03:02:27 -0300

    CONCLUSÃO PROJETO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) 
+        {
+            
+        }
+    
+    // TODO: Herdar da classe "Smartphone"
+    public override void InstalarAplicativo(string nome)
+    {
+        Console.WriteLine($"Instalando {nome} no Iphone.");
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
+}
+
 }
