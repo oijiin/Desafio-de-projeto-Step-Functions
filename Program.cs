23d0d447e79ecf3838c97298d953c224a2c4d9d8
EveeSilvaa <evellynsilvadeloper@gmail.com>
2024-10-21 13:17:27 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizei os testes com as classes Nokia e Iphone
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Criei um objeto do tipo Nokia
+        Nokia nokia = new Nokia("56789", "Nokia 3310", "123456789IMEI", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Criei um objeto do tipo Iphone
+        Iphone iphone = new Iphone("654321", "iPhone X", "987654321IMEI", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
