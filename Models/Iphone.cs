68986dced30590cc2339be89c087cebe19426d3b
sergiodonijr <sergiodonijr@gmail.com>
2024-10-30 22:07:52 -0300

    Tratamento nos Smartphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
+using System.Security.Claims;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no iPhone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", IMEI: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", iMEI: "222222222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
